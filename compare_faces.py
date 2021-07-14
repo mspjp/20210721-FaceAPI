8ea1a2cd11ae5d3c739336bd0e57d0c676db74ed
ShunIshii <ishiishun10@gmail.com>
2021-07-14 09:06:27 +0000

    Comment to compare_faces.py

--- compare_faces.py
+++ compare_faces.py
@@ -13,12 +13,12 @@ def main():
     global face_client
 
     try:
-        # Get Configuration Settings
+        # 環境変数の取得
         load_dotenv()
         cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
         cog_key = os.getenv('COG_SERVICE_KEY')
 
-        # Authenticate Face client
+        # Face client の認証
         credentials = CognitiveServicesCredentials(cog_key)
         face_client = FaceClient(cog_endpoint, credentials)
 
@@ -32,13 +32,13 @@ def main():
 def CompareFaces(image_1, image_2):
     print('Comparing faces in ', image_1, 'and', image_2)
 
-    # Determine if the face in image 1 is also in image 2
+    # image_1 にいる人が image_2 にいるかどうか確認
     with open(image_1, mode="rb") as image_data:
-        # Get the first face in image 1
+        # image_1 から1人の顔を取得
         image_1_faces = face_client.face.detect_with_stream(image=image_data)
         image_1_face = image_1_faces[0]
 
-        # Highlight the face in the image
+        # 顔をハイライトさせる
         fig = plt.figure(figsize=(8, 6))
         plt.axis('off')
         image = Image.open(image_1)
@@ -52,22 +52,22 @@ def CompareFaces(image_1, image_2):
         outputfile = 'face_to_match.jpg'
         fig.savefig(outputfile)
 
-    # Get all the faces in image 2
+    # image_2 に映っている全員の顔を取得
     with open(image_2, mode="rb") as image_data:
         image_2_faces = face_client.face.detect_with_stream(image=image_data)
         image_2_face_ids = list(map(lambda face: face.face_id, image_2_faces))
 
-        # Find faces in image 2 that are similar to the one in image 1
+        # image_2 の顔の中から、image_1 から取得した顔に似ている顔を取得
         similar_faces = face_client.face.find_similar(face_id=image_1_face.face_id, face_ids=image_2_face_ids)
         similar_face_ids = list(map(lambda face: face.face_id, similar_faces))
 
-        # Prepare image for drawing
+        # 描画の準備
         fig = plt.figure(figsize=(8, 6))
         plt.axis('off')
         image = Image.open(image_2)
         draw = ImageDraw.Draw(image)
 
-        # Draw and annotate matching faces
+        # マッチした顔をアノテーション
         for face in image_2_faces:
             if face.face_id in similar_face_ids:
                 r = face.face_rectangle
@@ -76,7 +76,7 @@ def CompareFaces(image_1, image_2):
                 draw.rectangle(bounding_box, outline='lightgreen', width=10)
                 plt.annotate('Match!',(r.left, r.top + r.height + 15), backgroundcolor='white')
 
-        # Save annotated image
+        # アノテーションされた画像の保存
         plt.imshow(image)
         outputfile = 'matched_faces.jpg'
         fig.savefig(outputfile)
